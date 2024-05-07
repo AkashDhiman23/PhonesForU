7d8820dafef579a7111295c30f01756f414ff222
AkashDhiman23 <akash.dhiman5555@gmail.com>
2024-05-07 14:00:56 +1200

    login and signup working with ajax

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 
 import os
 import psycopg2
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from model import db, User
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:dhiman223@localhost:5432/ecommercedb"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['STATIC_FOLDER'] = 'static'
 migrate = Migrate(app, db)
 db.init_app(app)
 
@@ -29,7 +31,7 @@ def signup():
 
     existing_user = User.query.filter_by(email_address=email).first()
     if existing_user:
-        return 'Email address already exists!'
+        return jsonify({'message': 'Email address already exists!'}), 400
     
     password_hash = generate_password_hash(password)
 
@@ -37,7 +39,7 @@ def signup():
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return jsonify({'message': 'Signup successful!'}), 200
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -46,9 +48,8 @@ def login():
 
     user = User.query.filter_by(email_address=email).first()
     if not user or not check_password_hash(user.password_hash, password):
-        return 'Invalid email or password'
+        return jsonify({'message': 'Invalid email or password'}), 401
 
-    return 'Logged in successfully'
-
+    return jsonify({'message': 'Logged in successfully'}), 200
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -10,8 +10,9 @@ class User(db.Model):
     user_lastname = db.Column(db.String(1000))
     email_address = db.Column(db.String(500))
     mobile = db.Column(db.String(30))
+    password_hash = db.Column(db.String(128), nullable=False)
     
     
     def __repr__(self):
-        return f"User( '{self.user_firstname},{self.user_lastnamename}', '{self.email_address}')"
+        return f"User( '{self.user_firstname},{self.user_lastname}', '{self.email_address}')"
  
