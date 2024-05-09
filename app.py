6fa56a946c3558e1da0c94d80701127899f4253f
AkashDhiman23 <akash.dhiman5555@gmail.com>
2024-05-09 13:27:47 +1200

    made new model for merchent login and signup and made all things working

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
-from model import db, User
+from model import db, User,MerchantUser
 
 
 app = Flask(__name__)
@@ -42,7 +42,7 @@ def signup():
 
     return jsonify({'success': True, 'message': 'Signup successful!'})
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['GET','POST'])
 def login():
     email = request.form['email']
     password = request.form['password']
@@ -52,5 +52,57 @@ def login():
         return jsonify({'message': 'Invalid email or password'}), 401
 
     return jsonify({'message': 'Logged in successfully'}), 200
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
+
+
+@app.route('/signupmerchant', methods=['POST'])
+def signupmerchant():
+    data = request.form
+    print("DATA IS:")
+    print(data)
+    existing_user = MerchantUser.query.filter_by(company_emailaddress=data['email']).first()
+    if existing_user:
+        return jsonify({'success': False, 'message': 'Email address is already registered.'}), 400
+    
+    password_hash = generate_password_hash(data['password'])
+    new_merchantuser = MerchantUser(
+        firstname=data['firstname'],
+        lastname=data['lastname'],
+        company_name=data['companyname'],
+        company_emailaddress=data['email'],
+        company_address=data['companyaddress'],
+        company_mobile=data['mobile'],
+        company_postcode=data['companypostcode'],
+        company_registrationno=data['companyregisterno'],
+        company_password_hash=password_hash
+    )
+    print(new_merchantuser)
+    db.session.add(new_merchantuser)
+    db.session.commit()
+    return jsonify({'success': True, 'message': 'Merchant signup successful!'})
+
+
+
+
+@app.route('/loginmerchant', methods=['POST'])
+def loginmerchant():
+    email = request.form['email']
+    password = request.form['password']
+
+    userMerchant = MerchantUser.query.filter_by(company_emailaddress=email).first()
+    if not userMerchant or not check_password_hash(userMerchant.company_password_hash, password):
+        return jsonify({'message': 'Invalid email or password'}), 401
+
+    return jsonify({'message': 'Logged in successfully'}), 200
 if __name__ == '__main__':
     app.run(debug=True)
+
--- model.py
+++ model.py
@@ -15,4 +15,20 @@ class User(db.Model):
     
     def __repr__(self):
         return f"User( '{self.user_firstname},{self.user_lastname}', '{self.email_address}')"
+    
+class MerchantUser(db.Model):
+    __tablename__ = 'MerchantUser'
+    company_id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(1000))
+    lastname = db.Column(db.String(1000))
+    company_name = db.Column(db.String(1000))
+    company_emailaddress = db.Column(db.String(500))
+    company_address = db.Column(db.String(500))
+    company_mobile = db.Column(db.String(30))
+    company_postcode = db.Column(db.String(500))
+    company_registrationno = db.Column(db.String(500))
+    company_password_hash = db.Column(db.String(1000))  
+    def __repr__(self):
+        return f"MerchantUser('{self.firstname}, {self.lastname}', '{self.company_emailaddress}')"
+
  
